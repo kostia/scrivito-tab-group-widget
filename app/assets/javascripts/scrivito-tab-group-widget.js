eb4e11b60dc6fbafe76fa0b58c18aa00ca041b86
Kostiantyn Kahanskyi <kostiantyn.kahanskyi@googlemail.com>
2015-05-13 19:22:43 +0200

    Togglable tab details
    Fixed #4

--- app/assets/javascripts/scrivito-tab-group-widget.js
+++ app/assets/javascripts/scrivito-tab-group-widget.js
@@ -14,6 +14,14 @@ scrivito.on('content', function(content) {
       });
     });
   });
+
+  $(content).find('[data-scrivito-tab-toggle-details]').on('click', function() {
+    var tab_details_link = $(this);
+    var tab_details_id = tab_details_link.attr('data-scrivito-tab-toggle-details');
+    var tab_details = tab_details_link.parent().find('.scrivito-tab-details-'+tab_details_id);
+    tab_details.toggle();
+    return false;
+  });
 });
 
 }());
--- app/models/tab_widget.rb
+++ app/models/tab_widget.rb
@@ -2,6 +2,10 @@ class TabWidget < Widget
   attribute :title, :string, default: 'New Tab'
   attribute :content, :widgetlist
 
+  attribute :font_size, :string, default: '14px'
+  attribute :font_color, :string, default: '#000000'
+  attribute :background_color, :string, default: '#FFFFFF'
+
   def self.valid_container_classes
     [TabGroupWidget]
   end
