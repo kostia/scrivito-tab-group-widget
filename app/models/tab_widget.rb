1724256e2d314b4facc926e0b047d2a7f6fd11c6
Kostiantyn Kahanskyi <kostiantyn.kahanskyi@googlemail.com>
2015-05-15 14:29:14 +0200

    Enum fields (and editor) for font size
    Fixes #8

--- app/models/tab_widget.rb
+++ app/models/tab_widget.rb
@@ -1,8 +1,10 @@
 class TabWidget < Widget
+  FONT_SIZES = (1..100).map { |i| "#{i}px" }
+
   attribute :title, :string, default: 'New Tab'
   attribute :content, :widgetlist
 
-  attribute :font_size, :string, default: '14px'
+  attribute :font_size, :enum, values: FONT_SIZES, default: '14px'
   attribute :font_color, :string, default: '#000000'
   attribute :background_color, :string, default: '#FFFFFF'
 
